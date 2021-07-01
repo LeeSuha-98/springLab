d2181821f7ae1a1ad392589a5ca3e0bfe89d2572
LeeSuha-98 <dltngk10@naver.com>
2021-07-01 17:19:20 +0900

    .

--- ex01Lab/src/test/java/org/zerock/controller/DataSourceTest.java
+++ ex01Lab/src/test/java/org/zerock/controller/DataSourceTest.java
@@ -11,8 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-	locations ={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@ContextConfiguration(locations ={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 public class DataSourceTest {
 
 	@Inject
